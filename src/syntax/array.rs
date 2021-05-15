d96830880c7d372e2d4faafc3c2d65eeff654eda
Seok Won <ikr@kakao.com>
2021-05-15 21:10:51 +0900

    Update array.rs: fixed size or ref

--- src/syntax/array.rs
+++ src/syntax/array.rs
@@ -10,4 +10,10 @@ fn main() {
     println!("{:?}", a); // [1, 2, 3, 4, 5]
     println!("{:?}", b); // [5, 4, 3, 2, 1]
     println!("{:?}", c); // [3, 3, 3, 3, 3]
+
+    give_me_fixed_arr([1, 2, 3, 4]); // error [E0308]
+    give_me_any_arr(&[1, 2, 3, 4]);
 }
+
+fn give_me_fixed_arr(a: [u8; 5]) {} // only take an array that has 5 elements
+fn give_me_any_arr(a: &[u8]) {} // doesn't have to care about size
